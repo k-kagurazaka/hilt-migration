786244389afc267790580d068d3f4b98235d6266
Keita Kagurazaka <kei.simple@gmail.com>
2021-10-02 23:15:47 +0900

    Clean up MainActivity

--- app/src/main/java/com/kkagurazaka/hilt/migration/ui/main/MainActivity.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/ui/main/MainActivity.kt
@@ -3,18 +3,10 @@ package com.kkagurazaka.hilt.migration.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kkagurazaka.hilt.migration.R
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), HasAndroidInjector {
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simple_activity)
