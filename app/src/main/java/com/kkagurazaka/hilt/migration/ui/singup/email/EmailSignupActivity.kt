6829b82c6b27f02cab321084d129b0b3f0375143
Keita Kagurazaka <kei.simple@gmail.com>
2021-10-02 23:13:48 +0900

    Clean up EmailSignupActivity and SnsSignupActivity

--- app/src/main/java/com/kkagurazaka/hilt/migration/ui/singup/email/EmailSignupActivity.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/ui/singup/email/EmailSignupActivity.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kkagurazaka.hilt.migration.R
 import com.kkagurazaka.hilt.migration.ui.singup.registration.UserRegistrationFragment
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class EmailSignupActivity : AppCompatActivity(), HasAndroidInjector {
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
-
+class EmailSignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simple_activity)
