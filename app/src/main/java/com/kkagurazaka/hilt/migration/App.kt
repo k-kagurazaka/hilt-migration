8f24dd825d05e883cf2ae45811587e44218ae817
Keita Kagurazaka <kei.simple@gmail.com>
2021-10-02 23:22:36 +0900

    Remove ApplicationComponent

--- app/src/main/java/com/kkagurazaka/hilt/migration/App.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/App.kt
@@ -1,13 +1,7 @@
 package com.kkagurazaka.hilt.migration
 
 import android.app.Application
-import com.kkagurazaka.hilt.migration.di.ApplicationComponent
-import dagger.hilt.EntryPoints
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-    val component: ApplicationComponent by lazy {
-        EntryPoints.get(this, ApplicationComponent::class.java)
-    }
-}
+class App : Application()
--- app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationComponent.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationComponent.kt
@@ -1,13 +1,8 @@
 package com.kkagurazaka.hilt.migration.di
 
 import dagger.Module
-import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-
-@InstallIn(SingletonComponent::class)
-@EntryPoint
-interface ApplicationComponent
 
 @InstallIn(SingletonComponent::class)
 @Module(
