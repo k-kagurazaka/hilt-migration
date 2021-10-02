74790a487c6f7cd0203614e40c5c169bf35545b0
Keita Kagurazaka <kei.simple@gmail.com>
2021-10-02 23:18:09 +0900

    Remove dagger.android codes from App and ApplicationComponent

--- app/src/main/java/com/kkagurazaka/hilt/migration/App.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/App.kt
@@ -2,22 +2,12 @@ package com.kkagurazaka.hilt.migration
 
 import android.app.Application
 import com.kkagurazaka.hilt.migration.di.ApplicationComponent
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import dagger.hilt.EntryPoints
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application(), HasAndroidInjector {
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
-
+class App : Application() {
     val component: ApplicationComponent by lazy {
         EntryPoints.get(this, ApplicationComponent::class.java)
     }
-
-    override fun androidInjector(): AndroidInjector<Any> =
-        androidInjector
 }
--- app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationComponent.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationComponent.kt
@@ -1,21 +1,17 @@
 package com.kkagurazaka.hilt.migration.di
 
-import com.kkagurazaka.hilt.migration.App
 import dagger.Module
-import dagger.android.AndroidInjectionModule
-import dagger.android.AndroidInjector
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @InstallIn(SingletonComponent::class)
 @EntryPoint
-interface ApplicationComponent : AndroidInjector<App>
+interface ApplicationComponent
 
 @InstallIn(SingletonComponent::class)
 @Module(
     includes = [
-        AndroidInjectionModule::class,
         ApplicationModule::class,
         RepositoryModule::class,
     ]
