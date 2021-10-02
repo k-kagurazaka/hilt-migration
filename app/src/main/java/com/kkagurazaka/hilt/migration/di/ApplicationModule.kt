5eb3f504250a59c0c10a169ea2d2822f00f01310
Keita Kagurazaka <kei.simple@gmail.com>
2021-10-02 23:24:18 +0900

    Enable @InstallIn check

--- app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationModule.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/di/ApplicationModule.kt
@@ -3,8 +3,11 @@ package com.kkagurazaka.hilt.migration.di
 import com.thirdparty.ThirdPartyLibrary
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 class ApplicationModule {
     @Singleton
--- app/src/main/java/com/kkagurazaka/hilt/migration/di/RepositoryModule.kt
+++ app/src/main/java/com/kkagurazaka/hilt/migration/di/RepositoryModule.kt
@@ -4,7 +4,10 @@ import com.kkagurazaka.hilt.migration.domain.UserRepository
 import com.kkagurazaka.hilt.migration.infra.UserRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
+@InstallIn(SingletonComponent::class)
 @Module
 interface RepositoryModule {
     @Binds
